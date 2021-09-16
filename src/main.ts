2ca4146a51d92452769c808a5e4c6383eae77b02
Timothy Cates <timothyccates1@gmail.com>
2021-09-16 14:02:23 -0700

    Added main.ts, right now it connects prits the subject and the attachment count

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import { Email } from "./email";
+import Imap from "imap";
+import mailParser from "mailparser";
+
+declare var process: {
+	env: {
+		EMAIL_HOST: string;
+		EMAIL_PORT: number;
+		EMAIL_USER: string;
+		EMAIL_PASS: string;
+	};
+};
+
+const EmailOptions: Imap.Config = {
+	host: process.env.EMAIL_HOST,
+	port: process.env.EMAIL_PORT,
+	user: process.env.EMAIL_USER,
+	password: process.env.EMAIL_PASS,
+	tls: true,
+	tlsOptions: {
+		servername: process.env.EMAIL_HOST, //sni for the servers that require it (gmail)
+	},
+};
+
+const email = new Email(EmailOptions);
+
+async function test() {
+	await email.connect();
+	let emails: mailParser.ParsedMail[] = await email.getEmails().catch((err) => {
+		throw err;
+	});
+	if (Array.isArray(emails)) {
+		for (let i = 0; i < emails.length; i++) {
+			const msg = emails[i];
+			console.log(`Email:\n ${msg.subject}\n
+		Body:\n${msg.text}\n
+		Attachments: [${msg.attachments.length}]
+		`);
+		}
+	}
+	email.disconnect();
+}
+
+test();
