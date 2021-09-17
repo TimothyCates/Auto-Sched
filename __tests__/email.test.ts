ac6b779257c06458bafe997e0c059bdb34a83860
Timothy Cates <timothyccates1@gmail.com>
2021-09-17 11:21:07 -0700

    Setup email test cases  outline

--- __tests__/email.test.ts
+++ __tests__/email.test.ts
@@ -1,73 +1,33 @@
-import * as Email from "../src/email";
 import Imap from "imap";
 import mailParser from "mailparser";
 
-declare var process: {
-	env: {
-		EMAIL_HOST: string;
-		EMAIL_PORT: number;
-		EMAIL_USER: string;
-		EMAIL_PASS: string;
-	};
-};
-
-const EmailOptions: Imap.Config = {
-	host: process.env.EMAIL_HOST,
-	port: process.env.EMAIL_PORT,
-	user: process.env.EMAIL_USER,
-	password: process.env.EMAIL_PASS,
-	tls: true,
-	tlsOptions: {
-		servername: process.env.EMAIL_HOST, //sni for the servers that require it (gmail)
-	},
-};
-
-const InvalidEmailOptions: Imap.Config = {
-	host: "null",
-	port: 993,
-	user: "user",
-	password: "pass",
-	tls: true,
-};
-
-describe("Email", () => {
-	it("Should create Imap with passed in values", () => {
-		const email = new Email.Email(InvalidEmailOptions);
-		const imap: any = email.getImap();
-		expect(imap._config.host).toEqual(InvalidEmailOptions.host);
-		expect(imap._config.port).toEqual(InvalidEmailOptions.port);
-		expect(imap._config.user).toEqual(InvalidEmailOptions.user);
-		expect(imap._config.password).toEqual(InvalidEmailOptions.password);
-		expect(imap._config.tls).toEqual(InvalidEmailOptions.tls);
+describe("The Email Class", () => {
+	describe("When Connecting", () => {
+		it("Should call imap.connect");
+		it("Errors Should call reject Promise");
+		it("SNI should be sent");
+		it("Connection options should be properly set");
+		it("Should reject if connection is already active");
 	});
-	it("Should authenticate with Imap Server", async () => {
-		const email = new Email.Email(EmailOptions);
-		await email.connect();
-		expect(email.getImap().state).toEqual("authenticated");
-		await email.disconnect();
+	describe("When Disconnecting", () => {
+		it("Should call imap.end");
+		it("Should resolve if no connection exists");
 	});
-	it("Should properly disconnect from server when disconnect it called", async () => {
-		const email = new Email.Email(EmailOptions);
-		await email.connect();
-		await email.disconnect();
-		expect(email.getImap().state).toEqual("disconnected");
+	describe("getEmails", () => {
+		it("should call fetchEmails");
+		it("should call parseEmails");
+		it("should resolve to an array");
+		it("should reject on error");
 	});
-	it("Should return a list of emails", async () => {
-		const email = new Email.Email(EmailOptions);
-		await email.connect();
-		let emails: mailParser.ParsedMail[] = await email.getEmails();
-		expect(Array.isArray(emails)).toBeTruthy();
-		await email.disconnect();
+	describe("readEmail", () => {
+		it("should join data chunks");
+		it("should resolve on stream end");
+		it("should reject on stream error");
 	});
-	it("Should return an error when accessing nonexistent mailbox", async () => {
-		const email = new Email.Email(EmailOptions);
-		await email.connect();
-		let emails: mailParser.ParsedMail[] = await email
-			.getEmails("sdfasdf")
-			.catch((err) => {
-				expect(err).toBeTruthy();
-				return [];
-			});
-		await email.disconnect();
+	describe("parseEmail", () => {
+		it("should resolve on empty input");
+		it("should resolve to array");
+		it("should call mailParser.simpleParser");
+		it("should reject on parse error");
 	});
 });
