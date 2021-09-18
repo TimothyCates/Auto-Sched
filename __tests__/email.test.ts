093ed0c704f9e3a8cfa326af4c3fea4e3a61d0b5
Timothy Cates <timothyccates1@gmail.com>
2021-09-18 13:52:18 -0700

    made email file wide, made emailClass an alias to (email as any) corrected the way I was checking for rejected promises reset all spys after all tests to prevent unsanitized results now that email is file wide when modifying the _imap.state value, ensure it is reset after the test when testing success and failures ensure we are mocking the imap emit events when testing a error ensure we are only checking for the fake error we created added missing getEmails test

--- __tests__/email.test.ts
+++ __tests__/email.test.ts
@@ -1,105 +1,110 @@
+import { catchClause } from "@babel/types";
 import Imap from "imap";
 import mailParser from "mailparser";
 import { resolve } from "path/posix";
 import { Email } from "../src/email";
 
+const email = new Email({
+	host: "host",
+	port: 55,
+	user: "user",
+	password: "pass",
+});
+afterAll(() => {
+	jest.restoreAllMocks();
+});
+
+const emailClass = email as any;
+
 describe("The Email Class", () => {
 	describe("When Connecting", () => {
-		it("Should call imap.connect", () => {
-			const email = new Email({ user: "", password: "" });
+		it("Should call imap.connect", async () => {
 			const spy = jest
-				.spyOn((email as any)._imap, "connect")
-				.mockImplementation(() => {});
-			email.connect();
+				.spyOn(emailClass._imap, "connect")
+				.mockImplementation(() => {
+					emailClass._imap.emit("ready");
+				});
+			await email.connect();
 			expect(spy).toHaveBeenCalled();
 		});
-		it("Errors Should call reject Promise", () => {
-			const email = new Email({ user: "", password: "" });
-			jest.spyOn((email as any)._imap, "connect").mockImplementation(() => {
-				(email as any)._imap.emit("error", "Connection Error");
+		it("Errors should cause a reject", async () => {
+			jest.spyOn(emailClass._imap, "connect").mockImplementation(() => {
+				emailClass._imap.emit("error", new Error("Connection Error"));
 			});
-			return expect(email.connect()).rejects.toBeDefined();
+			await expect(email.connect()).rejects.toThrow("Connection Error");
 		});
 		it("Should resolve when imap is ready", () => {
-			const email = new Email({ user: "", password: "" });
-			jest.spyOn((email as any)._imap, "connect").mockImplementation(() => {
-				(email as any)._imap.emit("ready", "Connection Completed");
+			jest.spyOn(emailClass._imap, "connect").mockImplementation(() => {
+				emailClass._imap.emit("ready", "Connection Completed");
 			});
 			return expect(email.connect()).resolves.toBeDefined();
 		});
 		it("SNI should be sent", () => {
-			const email = new Email({ host: "host", user: "", password: "" });
-			expect((email as any)._imap._config.tlsOptions.servername).toEqual(
-				"host"
-			);
+			expect(emailClass._imap._config.tlsOptions.servername).toEqual("host");
 		});
 		it("Connection options should be properly set", () => {
-			const email = new Email({
-				host: "host",
-				port: 55,
-				user: "user",
-				password: "pass",
-			});
-			expect((email as any)._imap._config.host).toEqual("host");
-			expect((email as any)._imap._config.port).toEqual(55);
-			expect((email as any)._imap._config.user).toEqual("user");
-			expect((email as any)._imap._config.password).toEqual("pass");
+			expect(emailClass._imap._config.host).toEqual("host");
+			expect(emailClass._imap._config.port).toEqual(55);
+			expect(emailClass._imap._config.user).toEqual("user");
+			expect(emailClass._imap._config.password).toEqual("pass");
 		});
-		it("Should reject if connection is already active", async () => {
-			const email = new Email({ user: "", password: "" });
-			jest.spyOn((email as any)._imap, "connect").mockImplementation(() => {
-				(email as any)._imap.state = "connected";
-				(email as any)._imap.emit("ready", "Connection Completed");
+		it("Should reject if connection already active", async () => {
+			jest.spyOn(emailClass._imap, "connect").mockImplementation(() => {
+				emailClass._imap.state = "connected";
+				emailClass._imap.emit("ready", "Connection Completed");
 			});
 			await email.connect();
-			return expect(email.connect()).rejects.toBeDefined();
+			await expect(email.connect()).rejects.toThrow("Already Connected");
+			emailClass._imap.state = "disconnected";
 		});
 	});
 	describe("When Disconnecting", () => {
-		it("Should call imap.end", () => {
-			const email = new Email({ user: "", password: "" });
-			(email as any)._imap.state = "connected"; //bypass already disconnected resolve
-			const spy = jest
-				.spyOn((email as any)._imap, "end")
-				.mockImplementation(() => {});
-			email.disconnect();
+		it("Should call imap.end", async () => {
+			emailClass._imap.state = "connected"; //bypass already disconnected resolve
+			const spy = jest.spyOn(emailClass._imap, "end").mockImplementation(() => {
+				emailClass._imap.emit("close");
+			});
+			await email.disconnect();
 			expect(spy).toHaveBeenCalled();
+			emailClass._imap.state = "disconnected";
 		});
-		it("Should resolve if no connection exists", () => {
-			const email = new Email({ user: "", password: "" });
-			return expect(email.disconnect()).resolves.toBeDefined();
+		it("Should resolve if no connection exists", async () => {
+			await expect(email.disconnect()).resolves.toBeDefined();
 		});
 	});
 	describe("getEmails", () => {
 		it("Should use inbox if none is specified", async () => {
-			const email = new Email({ user: "", password: "" });
 			jest
-				.spyOn((email as any)._imap, "openBox")
+				.spyOn(emailClass._imap, "openBox")
 				.mockImplementation((box, cb: any) => {
-					(email as any)._imap._box = box;
+					emailClass._imap._box = box;
 					cb();
 				});
+			jest.spyOn(emailClass, "_fetchEmails").mockImplementation(() => {
+				return Promise.resolve([""]);
+			});
+
 			await email.getEmails();
-			expect((email as any)._imap._box).toEqual("INBOX");
+			expect(emailClass._imap._box).toEqual("INBOX");
 		});
-		it("Should reject if errors opening mailbox", () => {
-			const email = new Email({ user: "", password: "" });
+		it("Should reject on openBox errors", async () => {
 			jest
-				.spyOn((email as any)._imap, "openBox")
+				.spyOn(emailClass._imap, "openBox")
 				.mockImplementation((_box, cb: any) => {
-					cb(true);
+					cb(new Error("Test Error"));
 				});
-			return expect(email.getEmails()).rejects.toBeDefined();
+			await email.getEmails().catch((error) => {
+				expect(error).toBeDefined();
+			});
 		});
 		it("Should call fetchEmails", async () => {
-			const email = new Email({ user: "", password: "" });
 			jest
-				.spyOn((email as any)._imap, "openBox")
+				.spyOn(emailClass._imap, "openBox")
 				.mockImplementation((_box, cb: any) => {
 					cb();
 				});
 			const spy = jest
-				.spyOn(email as any, "_fetchEmails")
+				.spyOn(emailClass, "_fetchEmails")
 				.mockImplementation(() => {
 					return Promise.resolve([""]);
 				});
@@ -107,18 +112,27 @@ describe("The Email Class", () => {
 			expect(spy).toHaveBeenCalled();
 		});
 		it("Should resolve to an array", async () => {
-			const email = new Email({ user: "", password: "" });
 			jest
-				.spyOn((email as any)._imap, "openBox")
+				.spyOn(emailClass._imap, "openBox")
 				.mockImplementation((_box, cb: any) => {
 					cb();
 				});
-			jest.spyOn(email as any, "_fetchEmails").mockImplementation(() => {
+			jest.spyOn(emailClass, "_fetchEmails").mockImplementation(() => {
 				return Promise.resolve([""]);
 			});
 			expect(await email.getEmails()).toBeInstanceOf(Array);
 		});
-		it("Should reject on fetch error", () => {});
+		it("Should reject on _fetch errors", async () => {
+			jest
+				.spyOn(emailClass._imap, "openBox")
+				.mockImplementation((_box, cb: any) => {
+					cb();
+				});
+			jest.spyOn(emailClass, "_fetchEmails").mockImplementation(() => {
+				return Promise.reject(new Error("Fetch Error"));
+			});
+			await expect(email.getEmails()).rejects.toThrow("Fetch Error");
+		});
 	});
 	describe("fetchEmails", () => {
 		it("should reject on error", () => {});
