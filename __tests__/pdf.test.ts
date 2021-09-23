a72db7655442a0011249f3b6745c821305ad0fb5
Timothy Cates <timothyccates1@gmail.com>
2021-09-23 11:14:06 -0700

    Added test cases for PDF class

---
+++ __tests__/pdf.test.ts
@@ -0,0 +1,14 @@
+describe("The PDF Class", () => {
+	describe("getPDF", () => {
+		it("Should resolve on pdfParser_dataReady", () => {});
+		it("Should reject on pdfParser_dataError", () => {});
+		it("Should reject if result is empty");
+	});
+	describe("getShifts", () => {
+		it("Produces array of shift objects from pdf json", () => {});
+	});
+	describe("getDaily", () => {
+		it("rejects if getPDF fails", () => {});
+		it("Sorts shifts into correct day", () => {});
+	});
+});
