43962b55b1647cebe8a3077ceec4e2318d7a847a
Timothy Cates <timothyccates1@gmail.com>
2021-09-25 12:30:54 -0700

    Fleshed out getDaily
    Added a DAY enum for converting date.getDay() to human readable,
    modified Day type to use a map for dynamic department names for shifts
    modified daily to allow undefined week for type checking
    Added _defaultDartmentHeads for department-position names
    Added _defaultBreakHeads for the deparment heads you want to gen breaks for
    Set the getPDF return type to Primise<PDFOUTPUT>
    Added _getDateFromDaily to abstract converting the header to date object
    Began First iteration of getDaily

--- src/pdf.ts
+++ src/pdf.ts
@@ -1,25 +1,98 @@
 import Pdf2json from "pdf2json";
+
+enum DAY {
+	Sunday,
+	Money,
+	Tuesday,
+	Wednesday,
+	Thursday,
+	Friday,
+	Saturday,
+}
 type Shift = {
 	name: string;
 	start: Date;
 	end: Date;
 };
 type Day = {
-	supervisorShifts: Shift[];
-	cashierShifts: Shift[];
-	courtesyClerkShifts: Shift[];
+	date: Date | undefined;
+	shifts: Map<String, Shift[]>;
 };
 type Daily = {
-	week: Date;
-	days: Day[];
+	week: Date | undefined;
+	days: [Day];
 };
 
 export class PDF {
+	private _defaultDepartmentHeads = [
+		"Front End Supervisor'P'",
+		"Cashier'-'",
+		"Backup - Cashier'-'",
+		"Self Checkout'U'",
+		"Courtesy Clerk'B'",
+		"Service Desk'D'",
+		"Utility Clerk'C'",
+		"Fuel'Z'",
+		"Bookkeeper'V'",
+	];
+
+	private _defaultBreakHeads = [
+		"Front End Supervisor'P'",
+		"Cashier'-'",
+		"Backup - Cashier'-'",
+		"Self Checkout'U'",
+		"Courtesy Clerk'B'",
+		"Service Desk'D'",
+		"Bookkeeper'V'",
+	];
 	private _pdf = new Pdf2json();
-	getDaily(fileLocation: string) {
-		return new Promise((resolve, reject) => {});
+	private _getDateFromDaily(text: string): Date {
+		const dateStrings = text.split(" ")[5].split("_");
+		return new Date(+dateStrings[3], +dateStrings[1] - 1, +dateStrings[2]);
 	}
-	getPDF(fileLocation: string) {
+	getDaily(
+		fileLocation: string,
+		options?: { departmentHeaders: string[]; breakHeaders: string[] }
+	) {
+		let daily: Daily;
+		return new Promise(async (resolve, reject) => {
+			try {
+				let chart = await this.getPDF(fileLocation);
+				for (let i = 0; i < chart.formImage.Pages.length; i++) {
+					let currDate;
+					let currDepartment;
+					const page = chart.formImage.Pages[i].Texts;
+					for (const text of page) {
+						const decoded = decodeURI(text.R[0].T);
+						//Get the chart's date and assign it to control variable
+						//Also set the week of variable
+						if (text.y <= 1 && text.x <= 54) {
+							const date = this._getDateFromDaily(decoded);
+							currDate = date.getDay();
+							if (daily.week === undefined) daily.week = date;
+							if (daily.days[currDate].date === undefined)
+								daily.days[currDate].date = date;
+						} else if (
+							text.x <= 17 &&
+							text.y >= 3 &&
+							decoded !== "Forecasted Headcount"
+						) {
+							if (
+								decoded in
+								(options?.departmentHeaders || this._defaultDepartmentHeads)
+							) {
+								currDepartment = decoded;
+							}
+						}
+					}
+				}
+				resolve(daily);
+			} catch (error) {
+				reject(error);
+			}
+		});
+	}
+	getPDF(fileLocation: string): Promise<Pdf2json.PDFOutput> {
 		return new Promise((resolve, reject) => {
 			this._pdf.on("pdfParser_dataError", (err) => reject(err.parserError));
 			this._pdf.on("pdfParser_dataReady", (output) => resolve(output));
