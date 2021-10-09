f66fd619bbe2b9668b9ed579858fbd2aeaf5e307
Timothy Cates <timothyccates1@gmail.com>
2021-10-09 00:51:58 -0700

    Convert class object to plain object
    Implement a way to recursivly convert objects made via classes to plain object for test

--- __tests__/dailly.test.ts
+++ __tests__/dailly.test.ts
@@ -5,6 +5,36 @@ const Mock = new Mocks();
 const daily = new Daily();
 const dailyClass = daily as any;
 
+const toObject = function (theClass: any) {
+	const original = theClass;
+	const keys = Object.keys(theClass);
+	return keys.reduce((classAsObj: any, key: any) => {
+		if (
+			typeof original[key] === "object" &&
+			original[key].hasOwnProperty("toObject")
+		)
+			classAsObj[key] = original[key].toObject();
+		else if (
+			typeof original[key] === "object" &&
+			original[key].hasOwnProperty("length")
+		) {
+			classAsObj[key] = [];
+			for (var i = 0; i < original[key].length; i++) {
+				if (
+					typeof original[key][i] === "object" &&
+					original[key][i].hasOwnProperty("toObject")
+				) {
+					classAsObj[key].push(original[key][i].toObject());
+				} else {
+					classAsObj[key].push(original[key][i]);
+				}
+			}
+		} else if (typeof original[key] === "function") {
+		} //do nothing
+		else classAsObj[key] = original[key];
+		return classAsObj;
+	}, {});
+};
 describe("The Daily Class", () => {
 	describe("_getShifts", () => {
 		it("Should take PdfText Array and output ShiftMap", () => {
@@ -37,11 +67,7 @@ describe("The Daily Class", () => {
 				return Mock.examplePDFData;
 			});
 			let output: any = await mockDaily.parseDaily("");
-			output.Days = [...output.Days];
-			output.Days.forEach((day: any, index: number) => {
-				output.Days[index] = { ...day };
-			});
-			output = { ...output };
+			output = toObject(output);
 			expect(output).toEqual(Mock.expectedParseOutput);
 		});
 	});
