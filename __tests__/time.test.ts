7ba7c3ac9c0d4cdd8f83709b07185b7282e8cfd3
Timothy Cates <timothyccates1@gmail.com>
2021-10-09 07:26:44 -0700

    Fix getFromString
    The Time class incorrectly calculated times over noon, and also did not round the results of the modulus resulting in time such as 29 minute instead of the correct 30 mins in cases where a floating point error occured

--- __tests__/time.test.ts
+++ __tests__/time.test.ts
@@ -1,35 +1,40 @@
-import { Time } from '../src/lib/time';
-describe('The Time Class', () => {
-	describe('getDateFromString', () => {
-		it('Should return correct date object from date string', () => {
+import { Time } from "../src/lib/time";
+describe("The Time Class", () => {
+	describe("getDateFromString", () => {
+		it("Should return correct date object from date string", () => {
 			let exampleDate = new Date(2021, 0, 1);
-			let dateString = 'Store Number Department Front End Day_01_01_2021';
+			let dateString = "Store Number Department Front End Day_01_01_2021";
 			expect(Time.getDateFromString(dateString)).toEqual(exampleDate);
 		});
-		it('Should Error if incorrect string is passed', () => {
+		it("Should Error if incorrect string is passed", () => {
 			expect(() => {
-				Time.getDateFromString('Invalid String');
-			}).toThrow('Invalid String');
+				Time.getDateFromString("Invalid String");
+			}).toThrow("Invalid String");
 		});
-		it('Should Error if incorrect date format is passed', () => {
+		it("Should Error if incorrect date format is passed", () => {
 			expect(() => {
-				Time.getDateFromString('Store Number Department Front End Day_01_billy_2021');
-			}).toThrow('Invalid Date');
+				Time.getDateFromString(
+					"Store Number Department Front End Day_01_billy_2021"
+				);
+			}).toThrow("Invalid Date");
 		});
 	});
-	describe('getFromString', () => {
+	describe("getFromString", () => {
 		let date = new Date(2021, 0, 1);
-		it('Should return correct time from time string', () => {
+		it("Should return correct time from time string", () => {
 			let expectedTime = new Date(2021, 0, 1, 6, 0, 0);
-			expect(Time.getFromString('600 A', date)).toEqual(expectedTime);
+			expect(Time.getFromString("600 A", date)).toEqual(expectedTime);
+			expect(Time.getFromString("1230 P", date)).toEqual(
+				new Date(2021, 0, 1, 12, 30)
+			);
 		});
-		it('Should return correct time from time string', () => {
+		it("Should return correct time from time string", () => {
 			expect(() => {
-				Time.getFromString('CheeserBurger A', date);
-			}).toThrow('Invalid Time');
+				Time.getFromString("CheeserBurger A", date);
+			}).toThrow("Invalid Time");
 			expect(() => {
-				Time.getFromString('9900 P', date);
-			}).toThrow('Invalid Time');
+				Time.getFromString("9900 P", date);
+			}).toThrow("Invalid Time");
 		});
 	});
 });
